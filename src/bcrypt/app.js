903f81471fc4630d94484f4e1e4eaf04123d6013
Serdar Büyüktemiz <hserdarb@gmail.com>
2013-10-30 15:05:32 +0200

    bcrypt sample account lock logic added

--- src/bcrypt/app.js
+++ src/bcrypt/app.js
@@ -4,7 +4,8 @@ var express = require('express'),
 
 mongoose.connect('mongodb://localhost/bcryptdemo');
 var user = mongoose.model('User', 
-						   mongoose.Schema({ email: String, name: String, pass: String }));
+						   mongoose.Schema({ email: String, name: String, pass: String, 
+						   					 loginTryCount: Number, lockedAt: Date }));
 
 var app = express();
 app.listen(3000);
@@ -38,36 +39,47 @@ app.post('/signup', function(req, res) {
 	var salt = bcrypt.genSaltSync(12);
 	var hash = bcrypt.hashSync(pass, salt);
 
-	var newUser = new user({ email: email, name: name, pass: hash});
+	var newUser = new user({ email: email, name: name, pass: hash, loginTryCount:0,lockedAt:null });
 	newUser.save(function (err, result) {
+		if (err) { res.sendfile('./views/failed.html'); };
+
 		req.session.user = {loggedIn : true, email : email, name:name};
 		res.redirect('/');
-	});
-
-    res.sendfile('./views/failed.html');
+	});    
 });
-
 
 app.post('/login', function(req, res) {
 
 	var email = req.body.email.trim().toLowerCase();
 	var pass = req.body.pass.trim();
 
-	user.findOne({email: email}, function(err, item) {		
-		if (bcrypt.compareSync(pass, item.pass)) {
-			
-			req.session.user = {loggedIn : true, email : email, name:item.name};
-			res.redirect('/');
+	user.findOne({email: email}, function(err, item) {
+		if (bcrypt.compareSync(pass, item.pass)
+			&& (item.lockedAt == null 
+				|| item.lockedAt < new Date().addHours(1))) {			
+
+			item.loginTryCount = 0;
+			item.lockedAt = null;
+			item.save(function (err) {
+
+				req.session.user = {loggedIn : true, email : email, name:item.name};
+				res.redirect('/');
+			});			
 		}
-		else{
-			res.sendfile('./views/failed.html');
+		else
+		{
+			item.loginTryCount++;
+			if (item.loginTryCount > 3) {
+				item.lockedAt = new Date();
+			};
+			item.save(function (err) {
+				res.sendfile('./views/failed.html');
+			});			
 		};        
     }); 
-  
-    
 });
 
 app.use(function(req, res){ res.send('Hello'); });
 
 String.prototype.trim=function(){return this.replace(/^\s+|\s+$/g, '');};
-
+Date.prototype.addHours= function(h){ this.setHours(this.getHours()+h); return this;}
